620fe677bb8c672a0220edc39561209edef18d5c
Carabalone <guilherme.caraba@gmail.com>
2022-05-11 18:33:47 -0300

    exer1

--- exer1.r
+++ exer1.r
@@ -1,10 +1,23 @@
 library(readxl)
 library(ggplot2)
+library(tidyr)
 
 data <- read_excel("./data/ResiduosPerCapita.xlsx")
 
-desired_countries <- list("EE - Estonia", "AT - Austria", "ES - Espanha")
+desired_countries <- c("EE - Estónia", "AT - Áustria", "ES - Espanha")
 
-names(data)[1] <- "pais"
-subset(data, exists(pais, desired_countries))
-desired_countries
+names(data)[1] <- "país"
+names(data)[2] <- "2004"
+names(data)[3] <- "2018"
+
+data <- data[data$país %in% desired_countries, ]
+data <- subset(data, select = c("país", "2004", "2018"))
+data <- pivot_longer(data, c('2004','2018'))
+names(data)[2] <- "ano"
+names(data)[3] <- "produção"
+
+ggplot(data, aes(x=país, fill=ano, y=produção)) +
+  geom_col(position = "dodge") +
+  ggtitle("Produção de Resíduos per capita") +
+  labs(x = "País", y = "Produção de Resíduos per capita")
+
